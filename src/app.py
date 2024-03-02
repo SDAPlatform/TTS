25c463b1bff544eadd7b8acf0014a17c8ebfb169
Eren Nevin <eren.m.nevin@gmail.com>
2024-03-02 18:45:11 +0300

    First version of text to speech

---
+++ src/app.py
@@ -0,0 +1,18 @@
+from gtts import gTTS
+import os
+
+def text_to_speech(text):
+    # Initialize gTTS with the text to convert
+    speech = gTTS(text)
+
+    # Save the audio file to a temporary file
+    speech_file = 'speech.mp3'
+    speech.save(speech_file)
+
+    # Play the audio file
+    os.system('afplay ' + speech_file)
+
+def main():
+    text_to_speech('Hello World')
+
+main()
